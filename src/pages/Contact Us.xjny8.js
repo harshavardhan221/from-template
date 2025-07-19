bcf2aea8ec663883544377c327391cf6dd444414
Harsha Vardhan <harshavardhan.firstcontact@gmail.com>
2025-07-19 22:21:32 +0530

    main branch code cleared

--- src/pages/Contact Us.xjny8.js
+++ src/pages/Contact Us.xjny8.js
@@ -1,44 +1,8 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
-import wixData from 'wix-data';
 
 $w.onReady(function () {
   // Initial load of all FAQs
-  wixData.query('Faq')
-    .find()
-    .then((res) => {
-      $w('#faqRepeater').data = res.items;
-    });
-
-  // Filter FAQs by search input (question only)
-  $w('#searchInput').onInput(() => {
-    const query = $w('#searchInput').value.toLowerCase();
-
-    wixData.query('Faq')
-      .contains('question', query)
-      .find()
-      .then((res) => {
-        $w('#faqRepeater').data = res.items;
-      });
-  });
-
-  // Accordion toggle setup
-  $w('#faqRepeater').onItemReady(($item, itemData) => {
-  // Set content manually
-  $item('#faqQuestion').text = itemData.question;
-  $item('#faqAnswer').text = itemData.answer;
-
-  // Start with answerBox collapsed
-  $item('#answerBox').collapse();
-
-  // Toggle on icon click
-  $item('#toggleIcon').onClick(() => {
-    const isCollapsed = $item('#answerBox').collapsed;
-    isCollapsed
-      ? $item('#answerBox').expand()
-      : $item('#answerBox').collapse();
-  });
-});
-
+  
 });
